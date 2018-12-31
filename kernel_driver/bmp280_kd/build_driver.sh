02f44f76ba119fc85384fffd90c9fde392f43d3e
khoitd1997 <khoidinhtrinh@gmail.com>
2018-12-31 01:44:49 -0800

    fix(driver): Changed insmod to modprobe for dependency issue

--- kernel_driver/bmp280_kd/build_driver.sh
+++ kernel_driver/bmp280_kd/build_driver.sh
@@ -10,5 +10,7 @@ source remove_driver.sh
 
 # install new driver
 make
+sudo ln -s ~/kernel_driver/bmp280_kd/bmp280_kd.ko /lib/modules/`uname -r`
+sudo depmod -a
 print_message "Installing New Driver"
-sudo insmod bmp280_kd.ko
+sudo modprobe bmp280_kd
--- kernel_driver/bmp280_kd/deploy_driver.sh
+++ kernel_driver/bmp280_kd/deploy_driver.sh
@@ -4,4 +4,5 @@ pi_address="pi@192.168.1.73"
 
 scp -r  ../../kernel_driver ${pi_address}:~
 # ; bash -l
-ssh -t ${pi_address} 'cd ~/kernel_driver/bmp280_kd; chmod u+x build_driver.sh; ./build_driver.sh; sleep 1; chmod u+x test_driver.sh; sudo ./test_driver.sh'
+ssh -t ${pi_address} 'cd ~/kernel_driver/bmp280_kd; chmod u+x build_driver.sh; ./build_driver.sh; sleep 1; ' 
+# chmod u+x test_driver.sh; sudo ./test_driver.sh'
